5a4feb518458291b3cd201e61302ba9bf65f12d1
prashanthrm74 <prashanthrm74@gmail.com>
2026-06-11 16:57:51 +0530

    Initial commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+
+
+const Banner = () => {
+  return (
+    <div className="w-full h-screen">
+        <img src="./banner.jpg" className="w-full h-full" />
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <>
+    < div className="bg-gray-400 flex">
+
+    <Logo/>
+       <Links/>
+
+    </div>
+    
+
+    </>
+  )
+}
+
+export default Navbar
+
+
+const Logo=()=>{
+
+    return(
+        <>
+        <div className="w-18 mx-5 ">
+            <img src="./Logo.png" />
+
+        </div>
+        </>
+    )
+}
+
+
+const Links=()=>{
+
+    return (
+        <>
+        <div className="flex" >
+
+            <Link className="bg-gray-800 text-white h-8 w-20 text-center p-1 rounded-xl m-5">Home</Link>
+            <Link className="bg-gray-800 text-white h-8 w-20 text-center p-1 rounded-xl m-5">Course</Link>
+            <Link className="bg-gray-800 text-white h-8 w-20 text-center p-1 rounded-xl m-5">Batch</Link>
+            <Link className="bg-gray-800 text-white h-8 w-20 text-center p-1 rounded-xl m-5">About</Link>
+            <Link className="bg-gray-800 text-white h-8 w-20 text-center p-1 rounded-xl m-5">Contact</Link>
+        
+        </div>
+        
+        </>
+    )
+
+}
